7bed335cc5739f0177fe99f4a34feda93da3ce3d
yz.esenali <61zat@mail.ru>
2017-02-03 11:31:11 +0600

    final database contacts

--- contacts.sql
+++ contacts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Янв 30 2017 г., 17:52
+-- Время создания: Фев 03 2017 г., 05:28
 -- Версия сервера: 5.7.11
 -- Версия PHP: 5.6.19
 
@@ -32,6 +32,7 @@ CREATE TABLE `contacts` (
   `INN` varchar(100) NOT NULL,
   `address_place` varchar(100) NOT NULL,
   `address_street` varchar(100) NOT NULL,
+  `detail_address` varchar(100) NOT NULL,
   `address_country` varchar(50) NOT NULL,
   `office` varchar(100) NOT NULL,
   `skype` varchar(100) NOT NULL,
@@ -43,8 +44,8 @@ CREATE TABLE `contacts` (
 -- Дамп данных таблицы `contacts`
 --
 
-INSERT INTO `contacts` (`id`, `legal_entity`, `INN`, `address_place`, `address_street`, `address_country`, `office`, `skype`, `facebook`, `e-mail`) VALUES
-(1, 'ООО «ЛР-Плюс1234»', '7730107052/773001001 ОГРН 1037739006689', 'QWE &quot;123456&quot;', 'Ул. Верейская 29,стр 154', 'Москва, 121357', '+7 (499) 951-03-20', 'elenablack3', 'www.facebook.com/pellinica', 'info@pellini.ru');
+INSERT INTO `contacts` (`id`, `legal_entity`, `INN`, `address_place`, `address_street`, `detail_address`, `address_country`, `office`, `skype`, `facebook`, `e-mail`) VALUES
+(1, 'ООО «ЛР-Плюс»', '7730107052/773001001 ОГРН 1037739006689', 'БЦ &quot;Верейская Плаза&quot;', 'Ул. Верейская 29,стр 154', 'офис 15', 'Москва, 1213578', '+7 (499) 951-03-20', 'elenablack', 'www.facebook.com/pellinica', 'info@pellini.ru');
 
 --
 -- Индексы сохранённых таблиц
